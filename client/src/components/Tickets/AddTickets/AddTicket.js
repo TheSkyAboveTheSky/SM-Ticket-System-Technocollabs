f8827e127bfbb2db69c98c26faac06afa00f1daa
TheSkyAboveTheSky <94720343+TheSkyAboveTheSky@users.noreply.github.com>
2022-08-29 03:09:07 +0000

    react : Tickets   v1.0

--- client/src/components/Tickets/AddTickets/AddTicket.js
+++ client/src/components/Tickets/AddTickets/AddTicket.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../../Sidebar/Sidebar";
 import Header from "../../Header/Header";
+import Axios from "../../Axios/Axios";
+import banner from "../../../assets/images/banner.jpg";
+import { AxiosError } from "axios";
 
 function AddTicket() {
+  const [id, setID] = useState("");
+  const [title, setTitle] = useState("");
+  const [agent, setAgent] = useState("");
+  const [username, setUsername] = useState("");
+  const [product, setProduct] = useState("");
+  const [details, setDetails] = useState("");
+  const [date, setDate] = useState();
+  const [priority, setPriority] = useState("");
+  const [departement, setDepartement] = useState("");
+
+  const createTicket = async () => {
+    try {
+      const response = await Axios.post("/ticket", {
+        id: id,
+        title: title,
+        agent: agent,
+        details: details,
+        userName: username,
+        Product: product,
+        departement: departement,
+        date: date,
+        Priority: priority,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <div>
       <>
@@ -55,10 +85,10 @@ function AddTicket() {
             <div className="container-fluid ">
               <div className="row d-flex justify-content-center align-items-center h-100">
                 <div className="col-md-9 col-lg-6 col-xl-5">
-                  <img src="" alt="login form" className="img-fluid" />
+                  <img src={banner} alt="login form" className="img-fluid" />
                 </div>
                 <div className="col-md-8 col-lg-6 col-xl-4">
-                  <form>
+                  <form onSubmit={createTicket}>
                     <div className="text-center mb-3">
                       <h3 classname="text-info">Add Tickets</h3>
                     </div>
@@ -68,6 +98,7 @@ function AddTicket() {
                           type="text"
                           className="form-control"
                           placeholder="ID"
+                          onChange={(e) => setID(e.target.value)}
                         />
                         <label>ID</label>
                       </div>
@@ -76,6 +107,7 @@ function AddTicket() {
                           type="text"
                           className="form-control"
                           placeholder="Title"
+                          onChange={(e) => setTitle(e.target.value)}
                         />
                         <label>Title</label>
                       </div>
@@ -86,6 +118,7 @@ function AddTicket() {
                           type="text"
                           className="form-control"
                           placeholder="Agent"
+                          onChange={(e) => setAgent(e.target.value)}
                         />
                         <label>Agent</label>
                       </div>
@@ -94,6 +127,7 @@ function AddTicket() {
                           type="text"
                           className="form-control"
                           placeholder="Username"
+                          onChange={(e) => setUsername(e.target.value)}
                         />
                         <label>Username</label>
                       </div>
@@ -104,6 +138,7 @@ function AddTicket() {
                           type="text"
                           className="form-control"
                           placeholder="Product"
+                          onChange={(e) => setProduct(e.target.value)}
                         />
                         <label>Product</label>
                       </div>
@@ -112,6 +147,7 @@ function AddTicket() {
                           type="text"
                           className="form-control"
                           placeholder="Departement"
+                          onChange={(e) => setDepartement(e.target.value)}
                         />
                         <label>Departement</label>
                       </div>
@@ -121,6 +157,7 @@ function AddTicket() {
                       <textarea
                         className="form-control"
                         placeholder="Details"
+                        onChange={(e) => setDetails(e.target.value)}
                       ></textarea>
                       <label>Details</label>
                     </div>
@@ -129,11 +166,15 @@ function AddTicket() {
                         className="form-control"
                         placeholder="Date"
                         type="date"
+                        onChange={(e) => setDate(e.target.value)}
                       />
                       <label>Date</label>
                     </div>
                     <div className="form-floating mb-3">
-                      <select className="form-select">
+                      <select
+                        className="form-select"
+                        onChange={(e) => setPriority(e.target.value)}
+                      >
                         <option selected>Choose...</option>
                         <option value="Low">Low</option>
                         <option value="Medium">Medium</option>
