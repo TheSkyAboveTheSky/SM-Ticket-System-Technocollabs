2640cc423cb429155c9f15d4cb7b659b0bcc5d0d
TheSkyAboveTheSky <94720343+TheSkyAboveTheSky@users.noreply.github.com>
2022-09-06 07:00:32 +0000

    react : style 403 404

--- client/src/components/SharedComponents/NotFound/NotFound.js
+++ client/src/components/SharedComponents/NotFound/NotFound.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./NotFound.min.css";
 
 function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <div id="NotFound">
+      <div class="mainbox">
+        <div className="err">4</div>
+        <i className="far fa-question-circle fa-spin"></i>
+        <div className="err2">4</div>
+        <div className="msg">
+          Maybe this page moved? Got deleted? Is hiding out in quarantine? Never
+          existed in the first place?
+          <p>
+            Let's go <a href="/">home</a> and try from there.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
